8f9858f4e0ca36188d5a2ba99428ced540960193
shubham_gumaste <shubham.gumaste@zycus.com>
2024-11-08 21:34:59 +0530

    added with loop

--- hackerEarth/factorial.js
+++ hackerEarth/factorial.js
@@ -1,8 +1,19 @@
-let num = 2;
+let num = 7;
+
+
+// program using the recursion function
 function isFactorial(num) {
     if (num == 0)
         return 1;
     return num * isFactorial(num - 1);
 }
 
+/* function isFactorial(num) {
+    // program using the iterative function
+    let result = 1;
+    for (let i = 2; i <= num; i++) {
+        result *= i;
+    }
+    return result;
+} */
 console.log(isFactorial(num));
